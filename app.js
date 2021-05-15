aa7b8f9e9154702cfbe17e450d35a9fc4ba6cc69
Jason Ziegler <jason@insightfulautomation.com>
2021-05-15 16:25:42 -0500

    added about and project routes

--- app.js
+++ app.js
@@ -2,13 +2,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const {data} = require('./data/data.json');
 
 app.get('/', (req,res) => {
-    res.send('hello world');
+    res.send("My Portfolio");
+});
+
+app.get('/about', (req, res) => {
+    res.send("About Page");
+});
+
+app.get('/project/', (req, res) => {
+    res.send("Project Page");
 });
 
 app.listen(port, () => {
     console.log(`My Porfolio app listening localhost:${port}`);
-})
+});
 
 
